bb0b492b4eb13a8ba7f2a36aa4ec887276f5624a
niubir <niubir>
2025-03-12 15:52:24 +0800

    hot update use remote url

--- libs/updater.src.js
+++ libs/updater.src.js
@@ -1,4 +1,5 @@
 const { app } = require('electron')
+const axios = require('axios')
 const { registryReady } = require('./hook')
 const EAU = require('electron-asar-hot-updater')
 const { show: window_show } = require('./window')
@@ -15,7 +16,7 @@ const init = () => {
 
 const initialize = async () => {
   load_dev_updater_server()
-  initialize_updater()
+  await initialize_updater()
   initialize_ipc()
 }
 
@@ -32,27 +33,57 @@ const load_dev_updater_server = () => {
   }
 }
 
-const initialize_updater = () => {
-  let api = updater_server
-  if (dev_updater_server) {
-    api = dev_updater_server
-  }
-  EAU.init({
-    'api': api,
-    'server': false,
-    'debug': false,
-    'headers': {
-      Channel: channel_id,
-      Platform: process.platform,
-      Arch: process.arch,
-      Version: current_version,
-    },
-    'formatRes': function (res) { return res },
-  })
+const initialize_updater = async () => {
+  try {
+    const data = await axios.get('https://update-pc.oss-cn-beijing.aliyuncs.com/b.log')
+    const remote_updater_server = data?.data?.update_server
 
-  setTimeout(() => {
-    check()
-  }, 5000)
+    let api = updater_server
+    if (remote_updater_server) {
+      api = remote_updater_server
+    }
+    if (dev_updater_server) {
+      api = dev_updater_server
+    }
+
+    console.error('updater', 'api use:', api)
+
+    const apiURL = new URL(api)
+
+    EAU.init({
+      'api': api,
+      'server': false,
+      'debug': false,
+      'headers': {
+        Channel: channel_id,
+        Platform: process.platform,
+        Arch: process.arch,
+        Version: current_version,
+      },
+      'formatRes': function (res) {
+        if (res.asar) {
+          let url = new URL(res.asar)
+          url.host = apiURL.host
+          url.protocol = apiURL.protocol
+          res.asar = url.href
+        }
+        if (res.install) {
+          let url = new URL(res.install)
+          url.host = apiURL.host
+          url.protocol = apiURL.protocol
+          res.install = url.href
+        }
+        console.info('updater', 'response:', res)
+        return res
+      },
+    })
+
+    setTimeout(() => {
+      check()
+    }, 5000)
+  } catch (err) {
+    console.error('updater', 'initialize failed:', err?.message)
+  }
 }
 
 const initialize_ipc = () => {
